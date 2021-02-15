3b58f5f229a9f8365dea47903f03e77d2a8e73cd
小徐 <xiaoxu@xiaoxudeMacBook-Pro.local>
2021-02-14 22:34:02 -0500

    break the original calc_y into more detailed parts and add corresponding tests

--- line.py
+++ line.py
@@ -1,3 +1,13 @@
+def calc_slope(x1, x2, y1, y2):
+    slope = (y2 - y1) / (x2 - x1)
+    return slope
+
+
+def calc_delta(x1, y1, slope):
+    delta = y1 - slope * x1
+    return delta
+
+
 def calc_y(t1, t2, x):
     x1 = t1[0]
     y1 = t1[1]
@@ -8,7 +18,7 @@ def calc_y(t1, t2, x):
         return -1
     if (y1 == y2):
         return y1
-    slope = (y2 - y1) / (x2 - x1)
-    delta = y1 - slope * x1
+    slope = calc_slope(x1,x2,y1,y2)
+    delta = calc_delta(x1, y1, slope)
     y = slope * x + delta
     return y
